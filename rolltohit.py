0e50d7bd8654350d33dcd92b211a59fbf1ba2b69
Donaldh138984 <hollad3338@clarkstate.edu>
2026-03-03 19:38:50 -0500

    Update rolltohit.py

--- rolltohit.py
+++ rolltohit.py
@@ -2,4 +2,4 @@
 #Prompt the user how many dice are being rolled, and what the hit target is
 #Roll the correct number of dice, then display the results
 #This is a guided practice. Either follow with the video or your instructor will
-#go over this in class.
+#go over this in class
